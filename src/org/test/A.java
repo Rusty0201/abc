4d8790b98f55e8cd10882dcc758f899b569952cb
Rusty0201 <ashamano263@gmail.com>
2022-11-21 10:14:30 +0530

    send a push

---
+++ src/org/test/A.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class A {
+	
+	private void sample() {
+		System.out.println("abc");
+	}
+	public static void main(String[] args) {
+		A a = new A();
+		a.sample();
+	}
+
+}
